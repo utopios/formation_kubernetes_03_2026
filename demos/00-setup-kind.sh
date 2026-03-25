22a23de20869c34399463c57d82b592de3922c48
Val Lorentz <progval+github@progval.net>
2026-03-25 12:27:04 +0100

    Chemin absolu vers 01-namespaces.yaml
    Sinon le fichier n'est pas trouvé quand on lance le script depuis le dossier parent (eg. `bash demos/00-setup-kind.sh`)

--- demos/00-setup-kind.sh
+++ demos/00-setup-kind.sh
@@ -42,7 +42,7 @@ kubectl label node "${CLUSTER_NAME}-worker2" env=staging    --overwrite
 kubectl label node "${CLUSTER_NAME}-worker3" env=production --overwrite
 
 echo "[INFO] Création des namespaces..."
-kubectl apply -f 01-namespaces.yaml
+kubectl apply -f $(dirname $0)/01-namespaces.yaml
 
 echo "[INFO] Installation Metrics Server..."
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
